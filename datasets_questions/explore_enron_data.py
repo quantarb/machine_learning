f1f89e583b5152687ec2457cf1c39c5613bc4494
quantarb <jlee153232@gmail.com>
2016-07-18 17:04:33 -0400

    feat: finished dataset and question mini project

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -20,3 +20,12 @@ import pickle
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
 
+print "number of datapoints:", len(enron_data)
+print "number of features:", len(enron_data["SKILLING JEFFREY K"])
+poi = 0
+for p in enron_data:
+    poi += enron_data[p]['poi']
+print "number of poi:", poi 
+print "total stock value for James Prentice:", enron_data['PRENTICE JAMES']['total_stock_value']
+print "total messages from Wesley Colwell to Pois:", enron_data['COLWELL WESLEY']['from_this_person_to_poi']
+print "total exercised stock options by Jeff Skilling:", enron_data['SKILLING JEFFREY K']['exercised_stock_options']
